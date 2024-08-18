15e90a041ad9dc827e801bdb247a7355da5e6a0c
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-08-18 22:24:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let bracketCounter = {};
+
+  let bracketMap = {};
+
+  for (let [open, close] of bracketsConfig) {
+    bracketMap[close] = open;
+    if (open === close) {
+      bracketCounter[open] = 0;
+    }
+  }
+
+  for (let char of str) {
+    if (bracketCounter.hasOwnProperty(char)) {
+      if(bracketCounter[char] % 2 === 0) {
+        stack.push(char);
+      }
+      else {
+        if(stack.length === 0 || stack.pop() !== char) {
+          return false;
+        }
+      }
+      bracketCounter[char]++;
+    }
+    else if(Object.values(bracketMap).includes(char)) {
+      stack.push(char);
+    }
+    else if (bracketMap.hasOwnProperty(char)) {
+      if (stack.length === 0 || stack.pop() !== bracketMap[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
